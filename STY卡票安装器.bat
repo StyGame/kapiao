e738723f59e01a99cc5fd8a35342bd281e6e3e21
StyGame <95107682+StyGame@users.noreply.github.com>
2022-03-19 17:02:53 +0800

    更换签名 bat自动获取管理员权限
    更换签名 bat自动获取管理员权限

--- STY卡票安装器.bat
+++ STY卡票安装器.bat
@@ -1,3 +1,4 @@
+@echo off&(cd/d "%~dp0")&(cacls "%SystemDrive%\System Volume Information" >nul 2>&1)||(start "" mshta vbscript:CreateObject^("Shell.Application"^).ShellExecute^("%~snx0"," %*","","runas",1^)^(window.close^)&exit /b)
 chcp 936
 cls
 @echo off
